59413ff93352c945895acabef6adb4715010d15d
Tyler <dreamingimlatios@gmail.com>
2022-07-26 22:48:56 -0500

    Add Mac OS support and update version

--- src/acl_unity_defines.cpp
+++ src/acl_unity_defines.cpp
@@ -9,7 +9,7 @@ ACL_UNITY_API int getVersion()
 	// Sanity check
 	if (compressed_tracks_version16::latest == compressed_tracks_version16::v02_00_00)
 	{
-		return (2 << 20) | 4;
+		return (2 << 20) | 6;
 	}
 	
 	return -1;
