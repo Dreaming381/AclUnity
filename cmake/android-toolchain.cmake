2f43907f7bfb6b7ee63b8ba2d4267cc37bcd32b4
Bryn Dickinson <bryn.dickinson@gmail.com>
2023-11-23 21:36:19 +0000

    Fix package naming issue, add Arm 7 build target, statically link libc++

--- cmake/android-toolchain.cmake
+++ cmake/android-toolchain.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_SYSTEM_NAME Android)
 set(CMAKE_SYSTEM_VERSION 21) # API level
-set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
-set(CMAKE_ANDROID_STL_TYPE c++_shared)
+# set(CMAKE_ANDROID_ARCH_ABI arm64-v8a)
+set(CMAKE_ANDROID_STL_TYPE c++_static)
